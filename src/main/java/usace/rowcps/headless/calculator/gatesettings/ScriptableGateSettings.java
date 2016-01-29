a7d1b68d6f9b84adf8c3fb3abb3ce4afe4d5bf45
Ryan Ripken <ryan@rmanet.com>
2016-01-28 17:04:16 -0800

    headless gate interface.
    Copied from Perforce
     Change: 74824

--- src/main/java/usace/rowcps/headless/calculator/gatesettings/ScriptableGateSettings.java
+++ src/main/java/usace/rowcps/headless/calculator/gatesettings/ScriptableGateSettings.java
@@ -1,42 +1,17 @@
 package usace.rowcps.headless.calculator.gatesettings;
 
-import hec.data.location.LocationTemplate;
 import java.util.Date;
-import java.util.Set;
-import usace.rowcps.headless.calculator.AbstractScriptableCalc;
-import usace.rowcps.headless.interfaces.ScriptableCalc;
-import usace.rowcps.computation.common.IThreadedBlockRetriever;
-import usace.rowcps.regi.model.ManagerId;
-import usace.rowcps.regi.model.RegiDomain;
 
 /**
  *
  * @author ryan
  */
-class ScriptableGateSettings extends AbstractScriptableCalc implements ScriptableCalc
+interface ScriptableGateSettings
 {
 
-	public ScriptableGateSettings(RegiDomain regiDomain, ManagerId managerId)
-	{
-		super(regiDomain, managerId);
-	}
+	void createGateSettings(String officeId, String locationStr, Date startDate, Date end) throws Exception;
 
-	public void adjustGateSettings(String officeId, String locationStr, Date startDate, double val, double unit)
-	{
-		LocationTemplate locRef = new LocationTemplate(officeId, locationStr);
+	void createGateSettingsOutlet(String officeId, String locationStr, Date startDate, Date end, String outletId) throws Exception;
 
-
-		  Set<Date> modifiedDatesForCachedSettings = null;
-//
-		   IThreadedBlockRetriever completionCallbackTarget = null;
-		    final int MAXROWSTORETRIEVE = 35;
-
-//        GateCache commonGateCache = new GateCache(currentDayControl, getManagerId(), projectDescriptor, MAXROWSTORETRIEVE, completionCallbackTarget,
-//                modifiedDatesForCachedSettings);
-//
-//        commonGateCache.initCache();
-
-	
-	}
-
+	void createGateSettingsGroup(String officeId, String locationStr, Date startDate, Date end, String groupId) throws Exception;
 }
