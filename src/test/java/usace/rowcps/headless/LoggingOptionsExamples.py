0b1aa8e062b2e0a1d6292d3a904ba15ec438da8d
Ryan Miles <ryanm@rmanet.com>
2016-10-31 14:53:47 -0700

    Adding LoggingOptionsExamples.py to headless testing jython file collection
    Copied from Perforce
     Change: 84700

---
+++ src/test/java/usace/rowcps/headless/LoggingOptionsExamples.py
@@ -0,0 +1,17 @@
+# Description of: LoggingOptions.setDbMessageLevel(int level)
+#
+# Adds Time Series logging messages in the OracleTimeSeriesDaoImpl.  Recommended
+# level is 2, as this provides basic information about the time series
+# retrieval/storage.
+#
+# Message Level | Description
+# --------------|-------------------------------------------------------------------------------------------------------------------------------|
+# <=0           | Default value, does not do anything.  Lower values do not change behavior.                                                  |
+# 1             | Logs message when no data is found.  Logs message when data is found, how much was retrieved or stored, and how long it took. |
+# 2             | Adds message with name of time series, and the units to retrieve/store.                                                       |
+# 3             | Adds message with the current time.                                                                                           |
+# 4             | Adds message with first 10 dates and values from each time series.                                                            |
+# >4            | Same as 4, but shows all values retrieved from each time series.  Higher values do not change behavior.                       |
+# --------------|-------------------------------------------------------------------------------------------------------------------------------|
+
+LoggingOptions.setDbMessageLevel(2)
