4d7f99213990a3fa931724d3452a0524c5be6c56
Ryan Miles <ryanm@rmanet.com>
2016-11-08 12:02:16 -0800

    Merging changelist 85005 to dev.
    Copied from Perforce
     Change: 85006

--- install/RegiHeadless/examples/InflowCalcMultipleActions.py
+++ install/RegiHeadless/examples/InflowCalcMultipleActions.py
@@ -2,6 +2,39 @@
 from java.util import Calendar
 from java.util import TimeZone
 from java.util import GregorianCalendar
+from usace.rowcps.headless import LoggingOptions
+
+# Description of: LoggingOptions.setDbMessageLevel(int level)
+#
+# Adds Time Series logging messages in the OracleTimeSeriesDaoImpl.  Recommended
+# level is 2, as this provides basic information about the time series
+# retrieval/storage.
+#
+# Message Level | Description
+# --------------|-------------------------------------------------------------------------------------------------------------------------------|
+# <=0           | Default value, does not do anything.  Lower values do not change behavior.                                                  |
+# 1             | Logs message when no data is found.  Logs message when data is found, how much was retrieved or stored, and how long it took. |
+# 2             | Adds message with name of time series, and the units to retrieve/store.                                                       |
+# 3             | Adds message with the current time.                                                                                           |
+# 4             | Adds message with first 10 dates and values from each time series.                                                            |
+# >4            | Same as 4, but shows all values retrieved from each time series.  Higher values do not change behavior.                       |
+# --------------|-------------------------------------------------------------------------------------------------------------------------------|
+
+LoggingOptions.setDbMessageLevel(2)
+
+
+# Description of: LoggingOptions.setMetricsEnabled(boolean value)
+#
+# Enables or disables the storage of REGI's Metric data pertaining to the
+# performance of the application.  This is incredibly helpful for identifying
+# issues where the application takes an excessive amount of time to operate.
+#
+# Metrics also log the location of the files as an INFO message if they are
+# enabled.
+#
+# By default, Metrics are disabled.
+
+LoggingOptions.setMetricsEnabled(True)
 
 # this gets a scriptable Pool Percent object
 inflowCalc = registry.getCalculation(1.0, "Inflow")
