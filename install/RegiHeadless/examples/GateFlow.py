5e101bc7131afb5d786f1479719ac77f4237563c
Scott Burdick-Yahya <scott@rmanet.com>
2017-05-01 13:35:40 -0700

    Updated delimiter for script, removed extra commented out areas
    Copied from Perforce
     Change: 91168

--- install/RegiHeadless/examples/GateFlow.py
+++ install/RegiHeadless/examples/GateFlow.py
@@ -10,9 +10,9 @@ def compute_All_Flowgroups(officeID, location, startCal, endCal):
         print ''
 
 
-def compute_Single_Flowgroup(officeID, location, startCal, endCal):
+def compute_Single_Flowgroup(officeID, location, startCal, endCal, flowGroup):
     try:
-        gateCalc.computeFlowGroup(officeID, location, startCal.getTime(), endCal.getTime(), "Flow.{0}.Pump_Out_Total".format(location))
+        gateCalc.computeFlowGroup(officeID, location, startCal.getTime(), endCal.getTime(), "Flow.{0}.{1}".format(location, flowGroup))
     except:
 #         print "Error Computing Flow Group at {0} {1}".format(officeID, location)
         print ''
@@ -88,8 +88,10 @@ officeID = "SWF"
 # endDate
 # gateCalc.computeFlowGroup("SWF", "LEWT2",  startCal.getTime(), endCal.getTime(), "Flow.LEWT2.ConduitGate_Total")
 # By setting the following parameter to True, the following flow groups in the list FlowGroupList will all be calculated. Items can be commented out
-# or commented back in individually. To turn this option off, set the following parameter to False.
-calculateSingleFlowGroups = False
+# or commented back in individually. To turn this option off, set the following parameter to False. User can determine which flow group they want to calculate
+# by changing the "flowGroup" variable.
+calculateSingleFlowGroups = True
+flowGroup = "ConduitGate_Total"
 locationList = ["ACTT2",
                 "ALAT2",
                 "BDWT2",
@@ -140,7 +142,7 @@ locationList = ["ACTT2",
 # By setting the following parameter to True, all of the following flow groups in each location in the list locationList will all be calculated.
 # Items can be commented out or commented back in individually. To turn this option off, set the following parameter to False.
 
-calculateAllFlowGroups = True
+calculateAllFlowGroups = False
 FlowGroupList = ["ACTT2",
                  "ALAT2",
                  "BDWT2",
@@ -174,84 +176,15 @@ FlowGroupList = ["ACTT2",
                  ]
 
 
+if calculateSingleFlowGroups:
+    for location in locationList:
+        print "Now Running", location, "SINGLE"
+        compute_Single_Flowgroup(officeID, location, startCal, endCal, flowGroup)
+
 if calculateAllFlowGroups:
     for location in FlowGroupList:
         print "Now Running", location, "GROUP"
         compute_All_Flowgroups(officeID, location, startCal, endCal)
 
-if calculateSingleFlowGroups:
-    for location in locationList:
-        print "Now Running", location, "SINGLE"
-        compute_Single_Flowgroup(officeID, location, startCal, endCal)
 
 
-# gateCalc.computeAll("SWF", "ACTT2",  startCal.getTime(), endCal.getTime())
-# gateCalc.computeAll("SWF", "ALAT2",  startCal.getTime(), endCal.getTime())
-# gateCalc.computeAll("SWF", "BDWT2",  startCal.getTime(), endCal.getTime())
-# gateCalc.computeAll("SWF", "BLNT2",  startCal.getTime(), endCal.getTime())
-# gateCalc.computeAll("SWF", "BNBT2",  startCal.getTime(), endCal.getTime())
-# gateCalc.computeAll("SWF", "CLDL1",  startCal.getTime(), endCal.getTime())
-# gateCalc.computeAll("SWF", "DAWT2",  startCal.getTime(), endCal.getTime())
-# gateCalc.computeAll("SWF", "GGLT2",  startCal.getTime(), endCal.getTime())
-
-# #gateCalc.computeAll("SWF", "GNGT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "GPVT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "HORT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "JFNT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "JPLT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "JSPT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "LEWT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "LVNT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "PCTT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "RRLT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "FRHT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "SCLT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "SMCT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "SOMT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "STIT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "TXKT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "WTYT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "TRNT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "FFLT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "BPRT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "EAMT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "FLWT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "LLST2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "GBYT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "PSMT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "TBLT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "TBRT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "GPET2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "BSLT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "SAGT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeAll("SWF", "MSDT2",  startCal.getTime(), endCal.getTime())
-# #gateCalc.computeFlowGroup("SWF", "ACTT2",  startCal.getTime(), endCal.getTime(), "Flow.ACTT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "ALAT2",  startCal.getTime(), endCal.getTime(), "Flow.ALAT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "BDWT2",  startCal.getTime(), endCal.getTime(), "Flow.BDWT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "BLNT2",  startCal.getTime(), endCal.getTime(), "Flow.BLNT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "BNBT2",  startCal.getTime(), endCal.getTime(), "Flow.BNBT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "DAWT2",  startCal.getTime(), endCal.getTime(), "Flow.DAWT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "GGLT2",  startCal.getTime(), endCal.getTime(), "Flow.GGLT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "GNGT2",  startCal.getTime(), endCal.getTime(), "Flow.GNGT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "GPVT2",  startCal.getTime(), endCal.getTime(), "Flow.GPVT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "HORT2",  startCal.getTime(), endCal.getTime(), "Flow.HORT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "JPLT2",  startCal.getTime(), endCal.getTime(), "Flow.JPLT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "LEWT2",  startCal.getTime(), endCal.getTime(), "Flow.LEWT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "LVNT2",  startCal.getTime(), endCal.getTime(), "Flow.LVNT2.Pump_Out_Total")
-####gateCalc.computeFlowGroup("SWF", "PCTT2",  startCal.getTime(), endCal.getTime(), "Flow.PCTT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "RRLT2",  startCal.getTime(), endCal.getTime(), "Flow.RRLT2.Pump_Out_Total")
-####gateCalc.computeFlowGroup("SWF", "FRHT2",  startCal.getTime(), endCal.getTime(), "Flow.FRHT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "SCLT2",  startCal.getTime(), endCal.getTime(), "Flow.SCLT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "SMCT2",  startCal.getTime(), endCal.getTime(), "Flow.SMCT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "SOMT2",  startCal.getTime(), endCal.getTime(), "Flow.SOMT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "STIT2",  startCal.getTime(), endCal.getTime(), "Flow.STIT2.Pump_Out_Total")
-####gateCalc.computeFlowGroup("SWF", "TXKT2",  startCal.getTime(), endCal.getTime(), "Flow.TXKT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "WTYT2",  startCal.getTime(), endCal.getTime(), "Flow.WTYT2.Pump_Out_Total")
-####gateCalc.computeFlowGroup("SWF", "TRNT2",  startCal.getTime(), endCal.getTime(), "Flow.TRNT2.Pump_Out_Total")
-####gateCalc.computeFlowGroup("SWF", "FFLT2",  startCal.getTime(), endCal.getTime(), "Flow.FFLT2.Pump_Out_Total")
-####gateCalc.computeFlowGroup("SWF", "EAMT2",  startCal.getTime(), endCal.getTime(), "Flow.EAMT2.Pump_Out_Total")
-####gateCalc.computeFlowGroup("SWF", "FLWT2",  startCal.getTime(), endCal.getTime(), "Flow.FLWT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "LLST2",  startCal.getTime(), endCal.getTime(), "Flow.LLST2.Pump_Out_Total")
-####gateCalc.computeFlowGroup("SWF", "GBYT2",  startCal.getTime(), endCal.getTime(), "Flow.GBYT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "PSMT2",  startCal.getTime(), endCal.getTime(), "Flow.PSMT2.Pump_Out_Total")
-# #gateCalc.computeFlowGroup("SWF", "SAGT2",  startCal.getTime(), endCal.getTime(), "Flow.SAGT2.Pump_Out_Total")
