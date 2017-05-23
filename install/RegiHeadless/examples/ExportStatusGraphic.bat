5b7ef2467f93dd8769982676d026ecfd2432088e
Deziree Dizon <deziree@rmanet.com>
2017-05-23 06:35:51 -0700

    1642-2.0.2 Updating Batch file time zone
    Copied from Perforce
     Change: 92209

--- install/RegiHeadless/examples/ExportStatusGraphic.bat
+++ install/RegiHeadless/examples/ExportStatusGraphic.bat
@@ -1,2 +1,2 @@
 :: The headless scripts can also be executed from a bat file as demonstrated below
-..\jre64\bin\java.exe -Dhec.data.cwmsRating.RatingSet.databaseLoadMethod=eager -Djava.util.logging.config.file=../config/properties/logging.properties -cp ..\lib\*;..\lib\ext\*;..\lib\modules\*;..\lib\sys\* usace.rowcps.headless.RegiCLI -Drowcps.timezone=GMT -p ..\\examples\\credentials.properties -f ..\\examples\\StatusDemo.py
+..\jre64\bin\java.exe -Dhec.data.cwmsRating.RatingSet.databaseLoadMethod=eager -Djava.util.logging.config.file=../config/properties/logging.properties -cp ..\lib\*;..\lib\ext\*;..\lib\modules\*;..\lib\sys\* usace.rowcps.headless.RegiCLI -Drowcps.timezone=America/Chicago -p ..\\examples\\credentials.properties -f ..\\examples\\StatusDemo.py
