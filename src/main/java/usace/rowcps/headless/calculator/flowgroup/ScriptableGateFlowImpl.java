999cebbcbb0f399bfcf72ce4c4b5b28f76830ca0
Caleb DeChant <caleb@rmanet.com>
2017-11-29 12:58:26 -0800

    fixing headless bugs related to addition of offset constraint and release overrides for all flow groups. Changed the computations to rely on the same code as generate flows for consistency
    Copied from Perforce
     Change: 99759

--- src/main/java/usace/rowcps/headless/calculator/flowgroup/ScriptableGateFlowImpl.java
+++ src/main/java/usace/rowcps/headless/calculator/flowgroup/ScriptableGateFlowImpl.java
@@ -1,39 +1,26 @@
 package usace.rowcps.headless.calculator.flowgroup;
 
-import hec.data.DataSetException;
-import hec.data.UtcOffsetConst;
-import hec.data.location.LocationGroup;
 import hec.data.location.LocationTemplate;
-import hec.data.tx.DataSetTx;
 import hec.db.DbConnectionException;
 import hec.db.DbIoException;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TimeZone;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import usace.metrics.services.Metrics;
 import usace.metrics.services.MetricsServiceProvider;
-import usace.metrics.services.Timer;
 import usace.rowcps.computation.ITimeSeriesComputationResult;
-import usace.rowcps.computation.TimeSeriesComputationData;
 import usace.rowcps.computation.TimeSeriesComputationError;
 import usace.rowcps.computation.flowgroup.DbCommitFlowGroupCalc;
-import usace.rowcps.computation.flowgroup.FlowGroupCalc;
-import usace.rowcps.computation.flowgroup.FlowGroupComputationException;
-import usace.rowcps.computation.util.FlowGroupOverrideDataAdapterBase;
-import usace.rowcps.data.DefaultFlowGroup;
 import usace.rowcps.data.flowgroup.FlowGroupTimeSeries;
 import usace.rowcps.data.flowgroup.IFlowGroup;
 import usace.rowcps.data.project.AtProjectDescriptor;
+import usace.rowcps.data.project.IProject;
 import usace.rowcps.headless.calculator.AbstractScriptableCalc;
 import usace.rowcps.headless.interfaces.ScriptableCalc;
-import usace.rowcps.regi.model.AtFlowGroupManager;
-import usace.rowcps.regi.model.AtTimeSeriesManager;
+import usace.rowcps.regi.model.AtProjectManager;
 import usace.rowcps.regi.model.CacheUsage;
 import usace.rowcps.regi.model.ManagerId;
 import usace.rowcps.regi.model.OptionalParams;
@@ -60,90 +47,41 @@ public class ScriptableGateFlowImpl extends AbstractScriptableCalc implements Sc
 
 	@Override
 	public void computeAll(String officeId, String locationId, long startTime, long endTime) {
-		LocationTemplate projectLocRef = new LocationTemplate(officeId, locationId);
+            
+            try
+            {
+                Metrics metrics = MetricsServiceProvider.createMetrics(this.getClass().getSimpleName(), "computeAll");
+                OptionalParams options = new OptionalParams(metrics);
+                
+                AtProjectDescriptor projDesc = new AtProjectDescriptor();
+                projDesc.setProjectLocationRef(new LocationTemplate(officeId, locationId));
+                
+                RegiDomain currentProject = (RegiDomain) RegiDomain.getCurrentProject();
+        
+                LocationTemplate projectLocationRef = projDesc.getProjectLocationRef();
 
-		Metrics metrics = MetricsServiceProvider.createMetrics(this.getClass().getSimpleName(), "computeAll");
-		OptionalParams options = new OptionalParams(metrics);
-
-		AtFlowGroupManager flowGroupManager = regiDomain.getAtFlowGroupManager(getManagerId());
-		AtTimeSeriesManager atTimeSeriesManager = regiDomain.getAtTimeSeriesManager(getManagerId());
-
-		possiblyWarnAboutMilliseconds(startTime, endTime);
-
-		final TimeZone utcZone = null;//TimeZone.getTimeZone("UTC");
-		Date startDate = new Date(startTime);
-		Date endDate = new Date(endTime);
-
-		try (Timer.Context context = metrics.createTimer().start()) {
-			Map<IFlowGroup, LocationGroup> conduitGateFlowGroupMap = flowGroupManager.retrieveFlowGroups(projectLocRef, null, CacheUsage.NORMAL);
-
-			if (conduitGateFlowGroupMap != null && !conduitGateFlowGroupMap.isEmpty()) {
-				final int groupMapSize = conduitGateFlowGroupMap.size();
-				logger.log(Level.INFO, "Found {0} groups to compute for start:{1} to end:{2}.",
-						new Object[]{groupMapSize, startDate, endDate});
-				Set<Map.Entry<IFlowGroup, LocationGroup>> entrySet = conduitGateFlowGroupMap.entrySet();
-				if (entrySet != null && !entrySet.isEmpty()) {
-					FlowGroupCalc flowGroupCalc = new FlowGroupCalc();
-
-					int count = 0;
-					for (Map.Entry<IFlowGroup, LocationGroup> entry : entrySet) {
-						IFlowGroup flowGroup = entry.getKey();
-						LocationGroup locationGroup = entry.getValue();
-						count++;
-
-						if (isProjectTotalFlowGroup(flowGroup)) {
-							//Special case for Project, as we need to use the Release Overrides
-							computeReleaseOverrides(flowGroup, startTime, endTime, options, atTimeSeriesManager);
-
-							logger.log(Level.INFO, "Compute {0}/{1} Project Total Flow Group:{2} completed normally.",
-									new Object[]{count, groupMapSize, flowGroup.getId()});
-							continue;
-						}
-
-						try {
-							//				FlowGroupTimeSeries newFlowGroupTimeSeries = flowGroup.newFlowGroupTimeSeries(parameterType,
-							//			interval, duration, version,
-							//			intervalOffsetSeconds, startCal.getTime(), null);
-							logger.log(Level.INFO, "Computing {0}/{1} Group:{2}",
-									new Object[]{count, groupMapSize, flowGroup.getId()});
-                                                        List<FlowGroupTimeSeries> outputTimeSeries = flowGroup.getOutputTimeSeriesList();
-							Map<FlowGroupTimeSeries, ITimeSeriesComputationResult> calcTimeSeries
-									= flowGroupCalc.calcTimeSeries(getManagerId(), flowGroup, startTime, endTime, outputTimeSeries, utcZone, options, CacheUsage.NORMAL);
-//									= flowGroupCalc.calcTimeSeries(getManagerId(), flowGroup, startTime, endTime, utcZone, options, CacheUsage.NORMAL);
-							if (calcTimeSeries == null) {
-								// I think that this means there was an error...
-								logger.log(Level.SEVERE, "Compute {0}/{1} Group:{2} returned null.", new Object[]{count, groupMapSize, flowGroup.getId()});
-							} else {
-
-								if (isCompleteFailure(calcTimeSeries)) {
-									logger.log(Level.SEVERE, "Compute {0}/{1} Group:{2} Failed. ",
-											new Object[]{count, groupMapSize, flowGroup.getId()});
-								} else {
-									DbCommitFlowGroupCalc.storeToTimeSeriesManager(calcTimeSeries, atTimeSeriesManager);
-
-									int failureCount = failureCount(calcTimeSeries);
-									if (0 < failureCount) {
-										logger.log(Level.SEVERE, "Compute {0}/{1} Group:{2} completed with {3} Errors.",
-												new Object[]{count, groupMapSize, flowGroup.getId(), failureCount});
-									} else {
-										logger.log(Level.INFO, "Compute {0}/{1} Group:{2} completed normally.",
-												new Object[]{count, groupMapSize, flowGroup.getId()});
-									}
-								}
-							}
-
-							logErrors(calcTimeSeries, flowGroup);
-
-						} catch (FlowGroupComputationException | DataSetException ex) {
-							String message = String.format("Scripted compute for %i/%i group:%s encountered an exception.", new Object[]{count, groupMapSize, flowGroup.getId()});
-							logger.log(Level.SEVERE, message, ex);
-						}
-					}
-				}
-			}
-		} catch (DbConnectionException | DbIoException ex) {
-			logger.log(Level.SEVERE, null, ex);
-		}
+                AtProjectManager atProjectManager = currentProject.getAtProjectManager(getManagerId());
+                IProject iProject = atProjectManager.getIProject(projectLocationRef, CacheUsage.NORMAL);
+                TimeZone timeZone = iProject.getProjectTimeZone();
+                
+                possiblyWarnAboutMilliseconds(startTime, endTime);
+                
+                DbCommitFlowGroupCalc calc = new DbCommitFlowGroupCalc(projDesc);
+                calc.calcTimeSeries(getManagerId(),
+                        projDesc.getProjectLocationRef(),
+                        null,
+                        null,
+                        new Date(startTime),
+                        new Date(endTime),
+                        CacheUsage.NORMAL,
+                        options,
+                        logger,
+                        timeZone);
+                
+            } catch (DbIoException | DbConnectionException ex)
+            {
+                logger.log(Level.SEVERE, null, ex);
+            }
 	}
 
 	@Override
@@ -169,83 +107,42 @@ public class ScriptableGateFlowImpl extends AbstractScriptableCalc implements Sc
 
 	@Override
 	public void computeFlowGroup(String officeId, String locationId, long startTime, long endTime, String groupId) {
-		AtFlowGroupManager flowGroupManager = regiDomain.getAtFlowGroupManager(getManagerId());
-		AtTimeSeriesManager atTimeSeriesManager = regiDomain.getAtTimeSeriesManager(getManagerId());
-		Metrics metrics = MetricsServiceProvider.createMetrics(this.getClass().getSimpleName(), "computeFlowGroup");
-		OptionalParams options = new OptionalParams(metrics);
+		
+            try
+            {
+                Metrics metrics = MetricsServiceProvider.createMetrics(this.getClass().getSimpleName(), "computeAll");
+                OptionalParams options = new OptionalParams(metrics);
+                
+                AtProjectDescriptor projDesc = new AtProjectDescriptor();
+                projDesc.setProjectLocationRef(new LocationTemplate(officeId, locationId));
+                
+                RegiDomain currentProject = (RegiDomain) RegiDomain.getCurrentProject();
+        
+                LocationTemplate projectLocationRef = projDesc.getProjectLocationRef();
 
-		possiblyWarnAboutMilliseconds(startTime, endTime);
-
-		LocationTemplate projectLocRef = new LocationTemplate(officeId, locationId);
-		try {
-			Map<IFlowGroup, LocationGroup> conduitGateFlowGroupMap = flowGroupManager.retrieveFlowGroups(projectLocRef,
-					null, CacheUsage.NORMAL);
-
-			Set<Map.Entry<IFlowGroup, LocationGroup>> entrySet = conduitGateFlowGroupMap.entrySet();
-
-			FlowGroupCalc flowGroupCalc = new FlowGroupCalc();
-
-			for (Map.Entry<IFlowGroup, LocationGroup> entry : entrySet) {
-				IFlowGroup flowGroup = entry.getKey();
-				LocationGroup locationGroup = entry.getValue();
-
-				computeGroup(flowGroup, groupId, flowGroupCalc, startTime, endTime, options, atTimeSeriesManager);
-			}
-
-		} catch (DbConnectionException | DbIoException ex) {
-			logger.log(Level.SEVERE, null, ex);
-		}
-	}
-
-	public void computeGroup(IFlowGroup flowGroup, String groupId, FlowGroupCalc flowGroupCalc, long startTime, long endTime,
-			OptionalParams options, AtTimeSeriesManager atTimeSeriesManager) {
-		if (flowGroup.getId().equals(groupId)) {
-
-			Metrics metrics = options.getMetrics().createMetrics("computeGroup", flowGroup.getId());
-
-			possiblyWarnAboutMilliseconds(startTime, endTime);
-			
-			logger.log(Level.INFO, "Computing Flow Group:{0} start:{1} end:{2}.",
-					new Object[]{flowGroup.getId(), new Date(startTime), new Date(endTime)});
-
-			try (Timer.Context context = metrics.createTimer().start()) {
-				final TimeZone utcZone = null;
-
-				if (isProjectTotalFlowGroup(flowGroup)) {
-					//Computing the overrides also stores them in the TS Manager.
-					computeReleaseOverrides(flowGroup, startTime, endTime, options, atTimeSeriesManager);
-					logger.log(Level.INFO, "Compute Project Total Flow Group:{0} completed normally.", 
-							new Object[]{flowGroup.getId()});
-					return;
-				}           
-                                List<FlowGroupTimeSeries> outputTimeSeries = flowGroup.getOutputTimeSeriesList();
-				Map<FlowGroupTimeSeries, ITimeSeriesComputationResult> calcTimeSeries
-						= flowGroupCalc.calcTimeSeries(getManagerId(), flowGroup, startTime, endTime, outputTimeSeries, options, CacheUsage.NORMAL);
-
-				if (calcTimeSeries == null) {
-					logger.log(Level.SEVERE, " Group:{0} returned null.", new Object[]{flowGroup.getId()});
-				} else if (isCompleteFailure(calcTimeSeries)) {
-					logger.log(Level.SEVERE, "Compute Group:{0} Failed. ", new Object[]{flowGroup.getId()});
-				} else {
-					DbCommitFlowGroupCalc.storeToTimeSeriesManager(calcTimeSeries, atTimeSeriesManager);
-
-					int failureCount = failureCount(calcTimeSeries);
-					if (0 < failureCount) {
-						logger.log(Level.SEVERE, "Compute Group:{0} completed with {1} Errors.",
-								new Object[]{flowGroup.getId(), failureCount});
-					} else {
-						logger.log(Level.INFO, "Compute Group:{0} completed normally.",
-								new Object[]{flowGroup.getId()});
-					}
-				}
-
-				logErrors(calcTimeSeries, flowGroup);
-
-			} catch (FlowGroupComputationException | DataSetException ex) {
-				String message = String.format("Scripted compute for group:%s encountered an exception.", new Object[]{flowGroup.getId()});
-				logger.log(Level.WARNING, message, ex);
-			}
-		}
+                AtProjectManager atProjectManager = currentProject.getAtProjectManager(getManagerId());
+                IProject iProject = atProjectManager.getIProject(projectLocationRef, CacheUsage.NORMAL);
+                TimeZone timeZone = iProject.getProjectTimeZone();
+                
+                possiblyWarnAboutMilliseconds(startTime, endTime);
+                
+                DbCommitFlowGroupCalc calc = new DbCommitFlowGroupCalc(projDesc);
+                calc.calcTimeSeries(getManagerId(),
+                        projDesc.getProjectLocationRef(),
+                        null,
+                        groupId,
+                        new Date(startTime),
+                        new Date(endTime),
+                        CacheUsage.NORMAL,
+                        options,
+                        logger,
+                        timeZone);
+                
+            } catch (DbIoException | DbConnectionException ex)
+            {
+                logger.log(Level.SEVERE, null, ex);
+            }
+            
 	}
 
 	@Override
@@ -261,84 +158,6 @@ public class ScriptableGateFlowImpl extends AbstractScriptableCalc implements Sc
 	public void computeFlowGroup(String officeId, String[] locationIds, long startTime, long endTime, String groupId) {
 		for (String locationId : locationIds) {
 			computeFlowGroup(officeId, locationId, startTime, endTime, groupId);
-		}
-	}
-
-	private boolean isCompleteFailure(Map<FlowGroupTimeSeries, ITimeSeriesComputationResult> calcTimeSeries) {
-		boolean isEpicFail = true;
-		if (calcTimeSeries != null && !calcTimeSeries.isEmpty()) {
-			for (Map.Entry<FlowGroupTimeSeries, ITimeSeriesComputationResult> entry : calcTimeSeries.entrySet()) {
-				FlowGroupTimeSeries key = entry.getKey();
-				ITimeSeriesComputationResult value = entry.getValue();
-
-				if (value instanceof TimeSeriesComputationData) {
-					isEpicFail = false;
-					break;
-				}
-			}
-		}
-
-		return isEpicFail;
-	}
-
-	private int failureCount(Map<FlowGroupTimeSeries, ITimeSeriesComputationResult> calcTimeSeries) {
-		int failCount = 0;
-		if (calcTimeSeries != null && !calcTimeSeries.isEmpty()) {
-			for (Map.Entry<FlowGroupTimeSeries, ITimeSeriesComputationResult> entry : calcTimeSeries.entrySet()) {
-				FlowGroupTimeSeries key = entry.getKey();
-				ITimeSeriesComputationResult value = entry.getValue();
-
-				if (!(value instanceof TimeSeriesComputationData) || (value instanceof Exception)) {
-					failCount++;
-				}
-			}
-		}
-
-		return failCount;
-	}
-
-	private boolean isProjectTotalFlowGroup(IFlowGroup flowGroup) {
-		return flowGroup.getIdSuffix().equals(DefaultFlowGroup.PROJECT.getGroupSuffix());
-	}
-
-	private void computeReleaseOverrides(IFlowGroup flowGroup, long startTime, long endTime, OptionalParams params, AtTimeSeriesManager atMan) {
-		List<FlowGroupTimeSeries> outputTs = flowGroup.getOutputTimeSeriesList();
-
-		OptionalParams options = new OptionalParams(params.getMetrics().createMetrics("computeReleaseOverrides"));
-
-		Date startDate = new Date(startTime);
-		Date endDate = new Date(endTime);
-
-		possiblyWarnAboutMilliseconds(startDate, endDate);
-		logger.log(Level.INFO, "Computing Release Overrides for:{0} start:{1} end:{2}.",
-				new Object[]{flowGroup.getId(), startDate, endDate});
-
-		AtProjectDescriptor projDesc = new AtProjectDescriptor();
-		projDesc.setProjectLocationRef(flowGroup.getLocationRef());
-
-		FlowGroupOverrideDataAdapterBase adapter = new FlowGroupOverrideDataAdapterBase(projDesc, getManagerId());
-		for (FlowGroupTimeSeries fgts : outputTs) {
-			try {
-				int intervalOffsetInSeconds = atMan.retrieveUtcIntervalOffset(fgts.getDescriptionTx());
-
-				//default to 0 if it's not valid.
-				if (intervalOffsetInSeconds == UtcOffsetConst.NO_UTC_OFFSET || intervalOffsetInSeconds == UtcOffsetConst.UNDEFINED_UTC_OFFSET) {
-					intervalOffsetInSeconds = 0;
-				}
-
-				DataSetTx dstx = adapter.getMergedTimeSeries(flowGroup, new HashSet<IFlowGroup>(), fgts.getInterval(), fgts.getParameterTypeString(), startDate, endDate, null, null, null, options, intervalOffsetInSeconds, CacheUsage.NORMAL, true);
-
-				if (dstx == null) {
-					logger.log(Level.SEVERE, "Unable to compute {1}.{0} Time Series for flowgroup: {2}", new Object[]{fgts.getIntervalString(), fgts.getParameterTypeString(), flowGroup.toString()});
-				} else if (dstx.getNumberValues() == 0) {
-					logger.log(Level.SEVERE, "No data computed for {1}.{0} Time Series for flowgroup: {2}", new Object[]{fgts.getIntervalString(), fgts.getParameterTypeString(), flowGroup.toString()});
-				} else {
-					//This should flag the time series manager as modified, I believe this was being missed in the getMergedTimeSeries function.
-					atMan.setModified(true);
-				}
-			} catch (DbConnectionException | DbIoException | DataSetException ex) {
-				logger.log(Level.SEVERE, "Unable to compute " + fgts.toString() + ", see error log. ", ex);
-			}
 		}
 	}
 
