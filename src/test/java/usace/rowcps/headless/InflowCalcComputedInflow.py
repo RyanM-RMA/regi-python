a87525c50830a3a554fa78a4270c83da97d008ce
Ryan Miles <ryanm@rmanet.com>
2018-12-20 10:32:48 -0800

    1812-4D/E - Updating comments for computed inflow calc jython file.
    Copied from Perforce
     Change: 122535

--- src/test/java/usace/rowcps/headless/InflowCalcComputedInflow.py
+++ src/test/java/usace/rowcps/headless/InflowCalcComputedInflow.py
@@ -29,11 +29,14 @@ endCal.set(Calendar.DAY_OF_MONTH, 4)
 # 	PROJECT_RELEASES
 #
 # None is supported by setComputationStorageOptions as well, and clears out all storage options.
-#inflowCalc.setComputationStorageOptions(None)
 # 
 # inflowCalc.setComputationStorageOptions is entirely optional, and if it's not used no additional computed values will
 # be stored with the computed inflow
 # 
+# Example uses:
+#inflowCalc.setComputationStorageOptions(None)
+#inflowCalc.setComputationStorageOptions(InflowComputationStorageOption.EVAP_AS_FLOW)
+#inflowCalc.setComputationStorageOptions(InflowComputationStorageOption.PROJECT_RELEASES)
 inflowCalc.setComputationStorageOptions(InflowComputationStorageOption.EVAP_AS_FLOW, InflowComputationStorageOption.PROJECT_RELEASES)
 
 # This computes and saves inflow for EUFA in May 2018 given the computation options set above
