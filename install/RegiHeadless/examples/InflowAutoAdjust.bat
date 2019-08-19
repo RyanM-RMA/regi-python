90f009c8ca7fa300d2bb3030be9f09b23847e083
Ryan Miles <ryanm@rmanet.com>
2019-08-19 12:31:30 -0700

    1812-2C - Removing eager loading for headless batch files.
    Copied from Perforce
     Change: 133365

--- install/RegiHeadless/examples/InflowAutoAdjust.bat
+++ install/RegiHeadless/examples/InflowAutoAdjust.bat
@@ -1,2 +1,2 @@
 :: The headless scripts can also be executed from a bat file as demonstrated below
-..\jre64\bin\java.exe -Dhec.data.cwmsRating.RatingSet.databaseLoadMethod=eager -Djava.util.logging.config.file=../config/properties/logging.properties -cp ..\lib\*;..\lib\ext\*;..\lib\modules\*;..\lib\sys\* usace.rowcps.headless.RegiCLI -Drowcps.timezone=GMT -p ..\\examples\\credentials.properties -f ..\\examples\\InflowCalcAutoAdjust.py
+..\jre64\bin\java.exe -Djava.util.logging.config.file=../config/properties/logging.properties -cp ..\lib\*;..\lib\ext\*;..\lib\modules\*;..\lib\sys\* usace.rowcps.headless.RegiCLI -Drowcps.timezone=GMT -p ..\\examples\\credentials.properties -f ..\\examples\\InflowCalcAutoAdjust.py
--- install/RegiHeadless/examples/flow.bat
+++ install/RegiHeadless/examples/flow.bat
@@ -1,3 +1,3 @@
 @echo off  
 :: The headless scripts can also be executed from a bat file as demonstrated below
-..\jre64\bin\java.exe -Dhec.data.cwmsRating.RatingSet.databaseLoadMethod=eager -Djava.util.logging.config.file=../config/properties/logging.properties -cp ..\lib\*;..\lib\ext\*;..\lib\modules\*;..\lib\sys\* usace.rowcps.headless.RegiCLI -Drowcps.timezone=GMT -p ..\\examples\\credentials.properties -f ..\\examples\\GateFlow.py
+..\jre64\bin\java.exe -Djava.util.logging.config.file=../config/properties/logging.properties -cp ..\lib\*;..\lib\ext\*;..\lib\modules\*;..\lib\sys\* usace.rowcps.headless.RegiCLI -Drowcps.timezone=GMT -p ..\\examples\\credentials.properties -f ..\\examples\\GateFlow.py
