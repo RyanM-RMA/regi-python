44f46fa6d2b0a1b0078a75c6abed89dcfcc69cf6
Ryan Miles <ryanm@rmanet.com>
2019-08-30 09:30:24 -0700

    CWMSREGI-15105 - Removing dead code and adjusting comments.
    Copied from Perforce
     Change: 133808

--- install/RegiHeadless/examples/InflowCalcComputedInflow.py
+++ install/RegiHeadless/examples/InflowCalcComputedInflow.py
@@ -20,24 +20,6 @@ endCal.set(Calendar.YEAR, 2018)
 endCal.set(Calendar.MONTH, 4)
 endCal.set(Calendar.DAY_OF_MONTH, 4)
 
-# inflowCalc includes a setComputationStorageOptions function, which takes in either one or many
-# InflowComputationStorageOption values.  This is used by computeInflow to determine if it should save additional
-# computed values like evaporation as flow and project releases.
-# 
-# The InflowComputationStorageOption enum contains two values:
-# 	EVAP_AS_FLOW
-# 	PROJECT_RELEASES
-#
-# None is supported by setComputationStorageOptions as well, and clears out all storage options.
-# 
-# inflowCalc.setComputationStorageOptions is entirely optional, and if it's not used no additional computed values will
-# be stored with the computed inflow
-# 
-# Example uses:
-#inflowCalc.setComputationStorageOptions(None)
-#inflowCalc.setComputationStorageOptions(InflowComputationStorageOption.EVAP_AS_FLOW)
-#inflowCalc.setComputationStorageOptions(InflowComputationStorageOption.PROJECT_RELEASES)
-inflowCalc.setComputationStorageOptions(InflowComputationStorageOption.EVAP_AS_FLOW, InflowComputationStorageOption.PROJECT_RELEASES)
-
-# This computes and saves inflow for EUFA in May 2018 given the computation options set above
+# This computes and saves inflow for EUFA in May 2018
+# computeInflow saves the Computed Inflow, Evaporation as Flow, and Project Total Flow Group time series data
 inflowCalc.computeInflow("SWT", "EUFA", startCal.getTime(), endCal.getTime())
