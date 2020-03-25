373f7603f5133453765e8f7b729b10d1887c939d
Ryan Miles <ryanm@rmanet.com>
2020-03-24 21:23:04 -0700

    1934-1 - Adding run shell script that performs the majority of the work necessary to run python scripts, and updating all of the shell scripts to support CWMS_HOME variables.
    Copied from Perforce
     Change: 140409

--- install/RegiHeadless-Solaris/examples/ExportBasinPie.sh
+++ install/RegiHeadless-Solaris/examples/ExportBasinPie.sh
@@ -1,30 +1,3 @@
 #!/bin/bash
-
-NOT_FOUND="notFound"
-
-ORA_INST=`getProp cwms.dbi.ConnectUsingUrl @${NOT_FOUND} dbi.properties|cut -d@ -f2`
-if [ $ORA_INST = $NOT_FOUND ]
-then
-	echo "Unable to locate Oracle URL from Cwms configuration. Will be taken from credentials.properties"
-fi
-
-echo "Found Oracle URL: $ORA_INST"
-
-# Get the office id from cwms.properties using the CWMS getProp script
-OFFICE_ID=`getProp cwms.OfficeID ${NOT_FOUND} cwms.properties $CWMS_HOME/config/properties`
-if [ $OFFICE_ID = $NOT_FOUND ]
-then
-	echo "Unable to locate OfficeID from Cwms configuration. Will be taken from credentials.properties"
-fi
-
-echo "Found Office ID: $OFFICE_ID"
-
-# The password file is expected to be at the following (full) path.
-PASS_FILE=$CWMS_HOME/config/properties/dbi.conf
-if [ ! -f $PASS_FILE ]
-then
-	echo "Unable to locate password file in Cwms configuration. Will be taken from credentials.properties"
-fi
-
-../jre64/bin/java -cp ../lib/*:../lib/ext/*:../lib/regi/*:../lib/cwms/*:../lib/sys/*: -Djava.library.path=../lib64 -DPLUGINS=../lib/ext -Doracle.metrics.clientid="CWMS REGI-Headless-v3.3" -Dcwms.db.impl.classpaths=../runtimeclassloader/cwmsDbApi/jooq -Doracle.url=$ORA_INST -Doracle.officeId=$OFFICE_ID -Dhec.passwd=$PASS_FILE -Djava.util.logging.config.file=../config/properties/logging.properties usace.rowcps.headless.RegiCLI -Drowcps.timezone=America/Chicago -p ..//examples//credentials.properties -f ..//examples//BasinPie.py
+./RunScript.sh -f ..//examples//BasinPie.py
 
